c5a702f3bc740dfa50f1925ae3398f55360e2436
Tom Gehrke <tgehrke@informatik.uni-bremen.de>
2013-07-04 17:55:59 +0200

    .cpp stub file

---
+++ iai_seminar_manipulation_controllers/src/IAISeminarMultiJointPositionController.cpp
@@ -0,0 +1,32 @@
+#include "../include/iai_seminar_manipulation_controllers/IAISeminarMultiJointPositionController.h"
+
+void IaiSeminarMultiJointPositionController::command_callback(const std_msgs::Float64MultiArrayConstPtr& msg)
+{
+
+}
+
+void IaiSeminarMultiJointPositionController::copy_from_command_buffer()
+{
+
+}
+
+bool IaiSeminarMultiJointPositionController::init(pr2_mechanism_model::RobotState *robot, ros::NodeHandle& n)
+{
+	return true;
+}
+
+void IaiSeminarMultiJointPositionController::starting()
+{
+
+}
+
+
+void IaiSeminarMultiJointPositionController::update()
+{
+
+}
+
+void IaiSeminarMultiJointPositionController::stopping()
+{
+
+}
